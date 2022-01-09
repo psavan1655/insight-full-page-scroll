e0220bac1ebb05a9e324f43e8618a952b4c9dcc8
Creole Studios <savan.patel@creolestudios.com>
2022-01-09 22:09:50 +0530

    desktop complete with touch

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var globalWheelCount = 0;
 const pg1TxtBx1 =
   "<img src='./assets/whiteLogo.png' height='30px' width='100px'/> <br /><p class='pg1-contura-txt'> CONTOURA<sup style='font-size: 15px; top: -1rem'> &#174;</sup> </p>";
 const pg1TxtBx2 =
@@ -8,34 +7,34 @@ const pg1Img1 =
   "<div class='covervid-wrapper'>  <img src='./assets/01.png' alt='Sole' class='pg1-img' /></div>";
 
 const pg2TxtBx2 =
-  "  <div class='position-relative'> <div class='pg2-dynamic-txt'>    DYNAMIC HEEL CUP AND ARCH SUPPORT <br />    <p class='pg1-anatomically-desc-txt'>The insole design is based on more than 120,000 prescribed footscans, effectiveky fitting 85% of wearers. With podiatrist-designed arch support, a stabilizing heel cup, and high rebound with shape-retention foam, Contoura <sup> &#174;</sup> offers maximum comfort, performance, and support, tailored to your midsole design.</p>  </div> </div>";
+  "  <div > <div class='pg2-dynamic-txt'>    DYNAMIC HEEL CUP AND ARCH SUPPORT <br />    <p class='pg1-anatomically-desc-txt'>The insole design is based on more than 120,000 prescribed footscans, effectiveky fitting 85% of wearers. With podiatrist-designed arch support, a stabilizing heel cup, and high rebound with shape-retention foam, Contoura <sup> &#174;</sup> offers maximum comfort, performance, and support, tailored to your midsole design.</p>  </div> </div>";
 
 const pg2Vid2 =
-  "<div class='covervid-wrapper2'> <video class='vid01'  autoplay muted preload='auto'> <source src='./assets/videos/V1.webm' type='video/webm'/></video></div>";
+  "<div class='covervid-wrapper2'> <video class='vid01' poster='./assets/01.png'  autoplay muted preload='metadata'> <source src='./assets/videos/V1.webm' type='video/webm'/></video></div>";
 
 const pg3Vid3 =
-  "<div class='covervid-wrapper3'> <video class='vid01'  autoplay muted preload='auto'> <source src='./assets/videos/V2.webm' type='video/webm' /></video></div>";
+  "<div class='covervid-wrapper3'> <video class='vid01'  autoplay muted preload='metadata'> <source src='./assets/videos/V2.webm' type='video/webm' /></video></div>";
 
 const pg3TxtBx3 =
-  "<div class='position-relative'><div class='pg3-sustainable-txt'>SUSTAINABLE MATERIALS <br />  <p class='pg1-anatomically-desc-txt'>    Contoura <sup> &#174;</sup> features innovative polyurethane (PU) foam technology, which not only supports its ergonomic shape, but results in dramatic material savings. Because the foam is poured into a mold rather than into sheets that are later cut and discarded. we use substantially less amterial, power, and natural resources.  </p></div>";
+  "<div><div class='pg3-sustainable-txt'>SUSTAINABLE MATERIALS <br />  <p class='pg1-anatomically-desc-txt'>    Contoura <sup> &#174;</sup> features innovative polyurethane (PU) foam technology, which not only supports its ergonomic shape, but results in dramatic material savings. Because the foam is poured into a mold rather than into sheets that are later cut and discarded. we use substantially less amterial, power, and natural resources.  </p></div>";
 
 const pg4Vid4 =
-  "<div class='covervid-wrapper2'> <video class='vid01'  autoplay muted preload='auto'> <source src='./assets/videos/V4.webm' type='video/webm'/></video></div>";
+  "<div class='covervid-wrapper2'> <video class='vid01'  autoplay muted preload='metadata'> <source src='./assets/videos/V4.webm' type='video/webm'/></video></div>";
 
 const pg4TxtBx4 =
   "<img src='./assets/whiteLogo.png' height='30px' width='100px'/> <br /><p class='pg1-contura-txt'> CONTOURA<sup style='font-size: 15px; top: -1rem'> &#174;</sup> </p>";
 
 const pg5Vid5 =
-  "<div class='covervid-wrapper3'> <video class='vid01'  autoplay muted preload='auto'> <source src='./assets/videos/V4.webm' type='video/webm' /></video></div>";
+  "<div class='covervid-wrapper3'> <video class='vid01'  autoplay muted preload='metadata'> <source src='./assets/videos/V4.webm' type='video/webm' /></video></div>";
 
 const pg5TxtBx5 =
-  "<div class='position-relative'><div class='pg3-sustainable-txt'>OUT-OF-THE-BOX COMFORT FOR EVERYONE<br />  <p class='pg1-anatomically-desc-txt'>    Contoura <sup> &#174;</sup>offers the highest levels of comfort, performance, support, and stability on the market today. Dynamic arch support helps control pronation without restricting it, assisting with natural foot motion.</p></div>";
+  "<div><div class='pg3-sustainable-txt'>OUT-OF-THE-BOX COMFORT FOR EVERYONE<br />  <p class='pg1-anatomically-desc-txt'>    Contoura <sup> &#174;</sup>offers the highest levels of comfort, performance, support, and stability on the market today. Dynamic arch support helps control pronation without restricting it, assisting with natural foot motion.</p></div>";
 
 const pg5TxtBx5Upper =
   "<div class='pg5-uppertext-container'><img src='./assets/whiteLogo.png' height='30px' width='100px'/> <br /><p class=''> CONTOURA<sup style='font-size: 15px; top: -1rem'> &#174;</sup> </p></div>";
 
 const pg6TxtBx6 =
-  "  <div class='position-relative'> <div class='pg6-customisable-txt'>    DYNAMIC HEEL CUP AND ARCH SUPPORT <br />    <p class='pg1-anatomically-desc-txt'>The insole design is based on more than 120,000 prescribed footscans, effectiveky fitting 85% of wearers. With podiatrist-designed arch support, a stabilizing heel cup, and high rebound with shape-retention foam, Contoura <sup> &#174;</sup> offers maximum comfort, performance, and support, tailored to your midsole design.</p>  </div> </div>";
+  "  <div> <div class='pg6-customisable-txt'>    DYNAMIC HEEL CUP AND ARCH SUPPORT <br />    <p class='pg1-anatomically-desc-txt'>The insole design is based on more than 120,000 prescribed footscans, effectiveky fitting 85% of wearers. With podiatrist-designed arch support, a stabilizing heel cup, and high rebound with shape-retention foam, Contoura <sup> &#174;</sup> offers maximum comfort, performance, and support, tailored to your midsole design.</p>  </div> </div>";
 
 const pg6Img6 =
   "<div class='covervid-wrapper6'>  <img src='./assets/06.png' alt='Sole' class='pg6-img' /></div>";
@@ -44,18 +43,98 @@ const footer1 =
   "<footer class='footer py-3 bg-white footerTest'><div class='container d-flex flex-wrap flex-column'><p class='foamTitle'>FOAM OPTIONS</p><div class='d-flex flex-wrap flex-row justify-content-around'><div class='ctr-1'><div class='footer-container-img1'></div><div class='footer-container-desc'><div class='footer-container-desc-title'>BI025 Librium<sup> &trade;</sup> Foam <br /><p class='footer-container-desc-txt'>High density, shock absorbing foam with 25% plant-based compound, for all-day support and performance. Available in                  molded applications.                </p>              </div>            </div>          </div>          <div class='ctr-2'>            <div class='footer-container-img2'></div>            <div class='footer-container-desc'>              <div class='footer-container-desc-title'>                BI025 DuraStep<sup> &trade;</sup> Foam <br />                <p class='footer-container-desc-txt'>                  Durable, medium density, moisture-resistant foam with 25%                  plant-based compounds for dynamic comfort. Available in molded                  applications.                </p>              </div>           </div>          </div>          <div class='ctr-3'>            <div class='footer-container-img3'></div>            <div class='footer-container-desc'>              <div class='footer-container-desc-title'>                BI011 Librium<sup> &trade;</sup> Foam <br />                <p class='footer-container-desc-txt'>                  High density, shock absorbing foam with 11% plant-based                  compound, for all-day support and performance. Available in                  molded applications.                </p>            </div>            </div>          </div>        </div>      </div>       ";
 
 const footer2 =
-  "<div class='btm-bar-footer d-flex flex-wrap flex-row justify-content-around align-items-center'>        <div class='btm-bar-footer-txt'>          SEE CONTOURA<sup> &reg;</sup> UP CLOSE.        </div>        <div class='btm-bar-footer-sample'>REQUEST A SAMPLE</div>      </div>     <div class='d-flex flex-wrap flex-column bg-dark p-5'>        <div class='d-flex flex-wrap flex-row justify-content-between text-white'        >          <div>            <div></div>            <div>IT'S WHAT'S INSIDE THAT COUNTS</div>          </div>          <div class='socialmedia-container d-flex flex-row'>            Follow us            <div class='footer-socialmedia-icon'>              &nbsp;              <i class='fab fa-linkedin-in'> </i>            </div>            <div class='footer-socialmedia-icon1'>              &nbsp;              <i class='fab fa-youtube'> </i>            </div>          </div>        </div>        <div class='d-flex flew-wrap flex-row text-white footer-policy'>          <div>Privacy Policy</div>          <div>Terms & Conditions</div>         <div>Site Map</div>          <div style='border: none !important'>Contact</div>        </div>        <div class='d-flex flex-wrap flex-column text-white footer-address'>          <div>&COPY; 2021 Matmarket, LLC. - All rights reserved</div>          <div>            Portsmouth, USA &#8226; Portland, USA &#8226; Birmingham, UK &#8226;            Hong Kong &#8226; Dongguan, China &#8226; Jinjiang, China &#8226;            Busan, Korea &#8226; Ho Chi Minn, Vietnam          </div>          <div>Website by fishnet</div>         </div>      </div>  </footer>";
+  "<div class='btm-bar-footer d-flex flex-wrap flex-row justify-content-around align-items-center'>        <div class='btm-bar-footer-txt'>          SEE CONTOURA<sup> &reg;</sup> UP CLOSE.        </div>        <div class='btm-bar-footer-sample'>REQUEST A SAMPLE</div>      </div>     <div class='d-flex flex-wrap flex-column bg-dark p-5'>        <div class='d-flex flex-wrap flex-row justify-content-between text-white'        >          <div>            <div></div>            <div>IT'S WHAT'S INSIDE THAT COUNTS</div>          </div>          <div class='socialmedia-container d-flex flex-row'>            Follow us            <div class='footer-socialmedia-icon'>              &nbsp;              <i class='fab fa-linkedin-in'> </i>            </div>            <div class='footer-socialmedia-icon1'>              &nbsp;  <i class='fab fa-youtube'> </i>            </div>          </div>        </div>        <div class='d-flex flew-wrap flex-row text-white footer-policy'>          <div>Privacy Policy</div>          <div>Terms & Conditions</div>         <div>Site Map</div>          <div style='border: none !important'>Contact</div>        </div>        <div class='d-flex flex-wrap flex-column text-white footer-address'>          <div>&COPY; 2021 Matmarket, LLC. - All rights reserved</div>          <div>            Portsmouth, USA &#8226; Portland, USA &#8226; Birmingham, UK &#8226;            Hong Kong &#8226; Dongguan, China &#8226; Jinjiang, China &#8226;            Busan, Korea &#8226; Ho Chi Minn, Vietnam          </div>          <div>Website by fishnet</div>         </div>      </div>  </footer>";
+
+var src = document.getElementById("pageone");
+var clientX = 0,
+  clientY = 0;
+var globalWheelCount = 0;
+
+src.addEventListener(
+  "touchstart",
+  function (e) {
+    // Cache the client X/Y coordinates
+    clientX = e.touches[0].clientX;
+    clientY = e.touches[0].clientY;
+  },
+  false
+);
+
+var deltaX = 0,
+  deltaY = 0;
+src.addEventListener(
+  "touchend",
+  function (e) {
+    deltaX = e.changedTouches[0].clientX - clientX;
+    deltaY = e.changedTouches[0].clientY - clientY;
+    console.log(deltaY);
+    let touchDirection = deltaY > 0;
+    if (touchDirection == false && deltaY < -100) {
+      if (globalWheelCount === 15) {
+        globalWheelCount = 15;
+      } else {
+        globalWheelCount += 1;
+      }
+      if (globalWheelCount == 1) {
+        $(".pg1-txt1").html(pg2TxtBx2);
+        $(".pg1-anatomically-txt").animate({ opacity: "hide" });
+        // $(".pg1-anatomically-txt").html("");
+        $(".flgshpConsole1").animate({ opacity: "hide" });
+        $("#img-container").html(pg2Vid2);
+      } else if (globalWheelCount == 2) {
+        $("#img-container").html(pg3Vid3);
+        $(".pg1-txt1").html(pg3TxtBx3);
+      } else if (globalWheelCount == 3) {
+        $("#img-container").html(pg4Vid4);
+        $(".pg1-txt1").html(pg4TxtBx4);
+      } else if (globalWheelCount == 4) {
+        $("#img-container").html(pg5Vid5);
+        $(".pg1-txt1").html(`${pg5TxtBx5}${pg5TxtBx5Upper}`);
+      } else if (globalWheelCount == 5) {
+        $(".pg1-txt1").html(pg6TxtBx6);
+        $("#img-container").html(pg6Img6);
+      } else if (globalWheelCount == 6) {
+        $("#footerContainer").html(`${footer1}${footer2}`);
+      }
+    } else if (touchDirection == true && deltaY > 100) {
+      if (globalWheelCount === 0) {
+        globalWheelCount = 0;
+      } else {
+        globalWheelCount -= 1;
+      }
+      if (globalWheelCount === 0) {
+        $(".one").animate({ left: "0", opacity: "show" });
+        $(".flgshpConsole1").animate({ opacity: "show" });
+        $(".pg1-anatomically-txt").animate({ opacity: "show" }).html(pg1TxtBx2);
+        $("#img-container").html(pg1Img1);
+        $(".pg1-txt1").html(pg1TxtBx1);
+      } else if (globalWheelCount == 1) {
+        $("#img-container").html(pg2Vid2);
+        $(".pg1-txt1").html(pg2TxtBx2);
+      } else if (globalWheelCount == 2) {
+        $("#img-container").html(pg3Vid3);
+        $(".pg1-txt1").html(pg3TxtBx3);
+      } else if (globalWheelCount == 3) {
+        $("#img-container").html(pg4Vid4);
+        $(".pg1-txt1").html(pg4TxtBx4);
+        $("#footerContainer").html("");
+      } else if (globalWheelCount == 4) {
+        $("#img-container").html(pg5Vid5);
+        $(".pg1-txt1").html(`${pg5TxtBx5}${pg5TxtBx5Upper}`);
+        $("#footerContainer").html(footer1);
+      } else if (globalWheelCount == 5) {
+        $(".pg1-txt1").html(pg6TxtBx6);
+        $("#img-container").html(pg6Img6);
+      } else if (globalWheelCount == 6) {
+      }
+    }
+
+    // Process the data ...
+  },
+  false
+);
 
 function detectMouseWheelDirection(e) {
-  // $(window).scroll(function () {
-  //   console.log("test");
-  //   alert("this works");
-  //   if ($("document").length) {
-  //     if ($(window).scrollTop() == 0 && e.deltaY > 0) {
-  //       $(".hero, .content").removeClass("scrolled");
-  //     }
-  //   }
-  // });
   var delta = null,
     direction = false;
   if (!e) {
@@ -135,8 +214,6 @@ function handleMouseWheelDirection(direction) {
     } else if (globalWheelCount == 6) {
     }
   } else {
-    console.log("testd demo");
-
     // this means the direction of the mouse wheel could not be determined
   }
 }
